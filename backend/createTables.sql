c0d6f9a0af57ebe52ff3167e481f242498f088cf
Lyndon Fan <lyndonfan@wessexinsights.com>
2024-01-03 13:18:06 +0000

    create views

--- backend/createTables.sql
+++ backend/createTables.sql
@@ -18,3 +18,50 @@ CREATE TABLE IF NOT EXISTS cards (
     next_review_at TIMESTAMP NOT NULL,
     FOREIGN KEY (deck_id) REFERENCES decks (id)
 );
+
+-- Create v_decks view
+
+CREATE VIEW IF NOT EXISTS v_decks AS
+WITH latest_decks AS (
+    SELECT
+        id,
+        MAX(updated_at) AS updated_at
+    FROM
+        decks
+    GROUP BY
+        id
+)
+SELECT
+    decks.*
+FROM
+    decks
+INNER JOIN
+    latest_decks
+ON
+    decks.id = latest_decks.id
+    AND decks.updated_at = latest_decks.updated_at
+;
+
+-- Create v_cards view
+CREATE VIEW IF NOT EXISTS v_cards AS
+WITH latest_cards AS (
+    SELECT
+        id,
+        deck_id,
+        MAX(updated_at) AS updated_at
+    FROM
+        cards
+    GROUP BY
+        id, deck_id
+)
+SELECT
+    cards.*
+FROM
+    cards
+INNER JOIN
+    latest_cards
+ON
+    cards.id = latest_cards.id
+    AND cards.deck_id = latest_cards.deck_id
+    AND cards.updated_at = latest_cards.updated_at
+;
